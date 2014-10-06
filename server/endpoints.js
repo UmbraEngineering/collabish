4d7f1fe49830707a9f3ef05392057abc994e803e
James Brumond <kbjr14@gmail.com>
2014-10-06 04:03:02 -0700

    added production flag when rendering client

--- server/endpoints.js
+++ server/endpoints.js
@@ -6,7 +6,8 @@ var templates  = require('./templates');
 app.use(function sendApp(req, res) {
 	res.send(200,
 		templates.render('src/index.hbs', {
-			static: conf.static.host
+			static: conf.static.host,
+			production: (process.env.NODE_ENV === 'production')
 		})
 	);
 });
