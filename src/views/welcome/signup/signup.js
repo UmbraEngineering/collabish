330b184de5a60c6da81e36a2181e6d3801f8cb01
James Brumond <kbjr14@gmail.com>
2014-10-08 03:29:28 -0700

    reenabled signup validation

--- src/views/welcome/signup/signup.js
+++ src/views/welcome/signup/signup.js
@@ -96,9 +96,9 @@ var SignupView = module.exports = View.extend({
 	signup: function() {
 		var self = this;
 
-		// if (! this.validate()) {
-			// return;
-		// }
+		if (! this.validate()) {
+			return;
+		}
 		this.showError();
 		this.disable(true);
 
